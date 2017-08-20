0237c289687f88467a3b520782e335ad03f10d61
Daniel Hughes <runlevelconsulting@yahoo.co.uk>
2017-08-20 12:25:58 +0100

    Adding GA code to ajax page

--- site/result.inc.php
+++ site/result.inc.php
@@ -111,3 +111,14 @@ if (is_numeric($_GET['fromId']) && is_numeric($_GET['toId'])){
   }
 }
 ?>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-92822223-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
